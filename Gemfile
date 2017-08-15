68c6b493ca47a5e9bd1461eb3b4d99c51e6c518c
Dewey Banks <DeweyBanks@gmail.com>
2017-08-15 14:51:33 -0400

    adds boostrap and users model

--- Gemfile
+++ Gemfile
@@ -26,7 +26,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 gem 'paperclip'
-
+gem 'bootstrap-sass'
 # Use Unicorn as the app server
 # gem 'unicorn'
 
@@ -47,5 +47,9 @@ group :development do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+end
+
+group :production do
+  gem 'rails_12factor'
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170815183351) do
+ActiveRecord::Schema.define(version: 20170815183650) do
 
   create_table "roles", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_digest"
+    t.integer  "role_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "users", ["role_id"], name: "index_users_on_role_id"
 
 end
